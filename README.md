ac331ef13e26a97e85a3581fa37bda4837eca5b2
Jhonatan Rodriguez <jhrodriguezi@unal.edu.co>
2023-05-21 16:50:15 -0500

    program adapted to a M/M/N system

--- README.md
+++ README.md
@@ -4,13 +4,19 @@ This is a workshop for the course "Stochastic Models" at Universidad Nacional de
 
 ## Description
 
-1. Program designed in C++ programming language for simulating M/M/1 queueing systems. This program can be used to perform various experiments and can be easily extended to other types of models.
+1. Program designed in C++ programming language for simulating M/M/N where N is the number of servers (we use only one queue). This program can be used to perform various experiments and can be easily extended to other types of models.
 
 2. Variable names were changed from English to Spanish.
 
 3. The input file "mm1.in" was changed to "param.txt" (for clarity and easy modification of input parameters).
 
-4. The output file "mm2.out" was changed to "result.txt" (for easier reading of statistics).
+    - The first line of the file contains the mean interarrival time (in minutes).
+    - The second line of the file contains the mean service time (in minutes).
+    - The third line of the file contains the maximum time (in minutes) to run the simulation.
+    - The fourth line of the file contains the maximum number of customers to generate.
+    - The fifth line of the file contains the number of servers.
+
+4. The output file "mm2.out" was changed to "result.txt" (for easier reading of statistics). 
 
 5. The file "lcgrand.cpp" was taken from http://www.sju.edu/~sforman/courses/2000F_CSC_5835/, which is a simulations website. Look for the Simlib.c link (a simulation library).
 
@@ -20,8 +26,11 @@ To execute the simulation, it is necessary to install the C++ programming langua
 
 ## Usage
 
+To execute the simulation, the main.cpp file must be compiled and executed. The result of the simulation will be stored in the "result.txt" file. By default, you can execute the simulation with the following command:
 
-To execute the simulation, the main.cpp file must be run.
+```bash
+./ejecutable.exe
+```
 
 ## License
 
@@ -29,4 +38,8 @@ To execute the simulation, the main.cpp file must be run.
 
 ## Authors
 
-Jhonatan Steven Rodriguez Ibañez; Edgar Daniel Gonzalez Diaz; Bryan Smith Colorado Lopez; Miguel Angel Puentes Cespedes; Juan Camilo Zambrano Lopez
+Jhonatan Steven Rodriguez Ibañez \
+Edgar Daniel Gonzalez Diaz \
+Bryan Smith Colorado Lopez \
+Miguel Angel Puentes Cespedes \
+Juan Camilo Zambrano Lopez
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,22 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
 #define LIMITE_COLA 100 /* Capacidad maxima de la cola */
 #define OCUPADO 1       /* Indicador de Servidor Ocupado */
 #define LIBRE 0         /* Indicador de Servidor Libre */
 
 int sig_tipo_evento, num_clientes_espera, num_esperas_requerido, num_eventos,
-    num_entra_cola, estado_servidor;
-float area_num_entra_cola, area_estado_servidor, media_entre_llegadas,
+    num_entra_cola, numero_servidores, servidor_salida;
+
+float area_num_entra_cola, media_entre_llegadas,
     media_atencion, tiempo_simulacion, tiempo_llegada[LIMITE_COLA + 1],
-    tiempo_ultimo_evento, tiempo_sig_evento[3], total_de_esperas;
+    tiempo_ultimo_evento, total_de_esperas, tiempo_simulacion_maxima;
+
+std::vector<float> tiempo_sig_evento, area_estado_servidor;
+std::vector<int> estado_servidores;
+
 FILE *parametros, *resultados;
 
 void inicializar(void);
@@ -23,6 +29,7 @@ void salida(void);
 void reportes(void);
 void actualizar_estad_prom_tiempo(void);
 float expon(float mean);
+float sum(std::vector<float> array);
 
 int main(void) /* Funcion Principal */
 {
@@ -33,12 +40,13 @@ int main(void) /* Funcion Principal */
 
   /* Especifica el numero de eventos para la funcion controltiempo. */
 
+  /* Lee los parametros de entrada. */
+
+  fscanf(parametros, "%f %f %f %d %d", &media_entre_llegadas, &media_atencion,
+         &tiempo_simulacion_maxima,
+         &num_esperas_requerido, &numero_servidores);
+
   num_eventos = 2;
-
-  /* Lee los parametros de enrtrada. */
-
-  fscanf(parametros, "%f %f %d", &media_entre_llegadas, &media_atencion,
-         &num_esperas_requerido);
 
   /* Escribe en el archivo de salida los encabezados del reporte y los
    * parametros iniciales */
@@ -48,7 +56,9 @@ int main(void) /* Funcion Principal */
           media_entre_llegadas);
   fprintf(resultados, "Tiempo promedio de atencion%16.3f minutos\n\n",
           media_atencion);
+  fprintf(resultados, "Tiempo de simulacion%16.3f minutos\n\n", tiempo_simulacion_maxima);
   fprintf(resultados, "Numero de clientes%14d\n\n", num_esperas_requerido);
+  fprintf(resultados, "Numero de servidores%14d\n\n", numero_servidores);
 
   /* iInicializa la simulacion. */
 
@@ -57,30 +67,29 @@ int main(void) /* Funcion Principal */
   /* Corre la simulacion mientras no se llegue al numero de clientes
    * especificaco en el archivo de entrada*/
 
-  while (num_clientes_espera < num_esperas_requerido) {
+  while (num_clientes_espera < num_esperas_requerido && tiempo_simulacion < tiempo_simulacion_maxima)
+  {
 
     /* Determina el siguiente evento */
-
     controltiempo();
 
-    /* Actualiza los acumuladores estadisticos de tiempo promedio */
-
+    /* Actualiza los acumuladores estadisticos de tiempos promedios */
     actualizar_estad_prom_tiempo();
 
     /* Invoca la funcion del evento adecuado. */
-
-    switch (sig_tipo_evento) {
+    switch (sig_tipo_evento)
+    {
     case 1:
       llegada();
       break;
     case 2:
-      salida();
       break;
+    default:
+      salida();
     }
   }
 
   /* Invoca el generador de reportes y termina la simulacion. */
-
   reportes();
 
   fclose(parametros);
@@ -97,7 +106,13 @@ void inicializar(void) /* Funcion de inicializacion. */
 
   /* Inicializa las variables de estado */
 
-  estado_servidor = LIBRE;
+  estado_servidores.resize(numero_servidores + 1);
+
+  for (int i = 1; i <= numero_servidores; i++)
+  {
+    estado_servidores[i] = LIBRE;
+  }
+
   num_entra_cola = 0;
   tiempo_ultimo_evento = 0.0;
 
@@ -106,13 +121,20 @@ void inicializar(void) /* Funcion de inicializacion. */
   num_clientes_espera = 0;
   total_de_esperas = 0.0;
   area_num_entra_cola = 0.0;
-  area_estado_servidor = 0.0;
+
+  area_estado_servidor.resize(numero_servidores + 1);
 
   /* Inicializa la lista de eventos. Ya que no hay clientes, el evento salida
      (terminacion del servicio) no se tiene en cuenta */
 
+  tiempo_sig_evento.resize(num_eventos + numero_servidores + 1);
+
   tiempo_sig_evento[1] = tiempo_simulacion + expon(media_entre_llegadas);
-  tiempo_sig_evento[2] = 1.0e+30;
+  tiempo_sig_evento[2] = tiempo_simulacion_maxima;
+  for (int i = 3; i <= num_eventos + numero_servidores; i++)
+  {
+    tiempo_sig_evento[i] = 1.0e+30;
+  }
 }
 
 void controltiempo(void) /* Funcion controltiempo */
@@ -124,15 +146,17 @@ void controltiempo(void) /* Funcion controltiempo */
 
   /*  Determina el tipo de evento del evento que debe ocurrir. */
 
-  for (i = 1; i <= num_eventos; ++i)
-    if (tiempo_sig_evento[i] < min_tiempo_sig_evento) {
+  for (i = 1; i <= num_eventos + numero_servidores; ++i)
+    if (tiempo_sig_evento[i] < min_tiempo_sig_evento)
+    {
       min_tiempo_sig_evento = tiempo_sig_evento[i];
       sig_tipo_evento = i;
     }
 
   /* Revisa si la lista de eventos esta vacia. */
 
-  if (sig_tipo_evento == 0) {
+  if (sig_tipo_evento == 0)
+  {
 
     /* La lista de eventos esta vacia, se detiene la simulacion. */
 
@@ -150,21 +174,33 @@ void llegada(void) /* Funcion de llegada */
 {
   float espera;
 
-  /* Programa la siguiente llegada. */
-
   tiempo_sig_evento[1] = tiempo_simulacion + expon(media_entre_llegadas);
 
-  /* Reisa si el servidor esta OCUPADO. */
+  int servidor_libre;
+  bool servidor_desocupado_encontrado = false;
 
-  if (estado_servidor == OCUPADO) {
+  /* Revisa si los servidores estan en modo OCUPADO. */
+  for (int i = 1; i <= numero_servidores; i++)
+  {
+    if (estado_servidores[i] == LIBRE)
+    {
+      servidor_libre = i;
+      servidor_desocupado_encontrado = true;
+      break;
+    }
+  }
 
-    /* Sservidor OCUPADO, aumenta el numero de clientes en cola */
+  if (servidor_desocupado_encontrado == false)
+  {
+
+    /* Servidores en modo OCUPADO, aumenta el numero de clientes en cola */
 
     ++num_entra_cola;
 
     /* Verifica si hay condici�n de desbordamiento */
 
-    if (num_entra_cola > LIMITE_COLA) {
+    if (num_entra_cola > LIMITE_COLA)
+    {
 
       /* Se ha desbordado la cola, detiene la simulacion */
 
@@ -180,10 +216,11 @@ void llegada(void) /* Funcion de llegada */
     tiempo_llegada[num_entra_cola] = tiempo_simulacion;
   }
 
-  else {
+  else
+  {
 
     /*  El servidor esta LIBRE, por lo tanto el cliente que llega tiene tiempo
-       de eespera=0 (Las siguientes dos lineas del programa son para claridad, y
+       de espera=0 (Las siguientes dos lineas del programa son para claridad, y
        no afectan el reultado de la simulacion ) */
 
     espera = 0.0;
@@ -192,11 +229,12 @@ void llegada(void) /* Funcion de llegada */
     /* Incrementa el numero de clientes en espera, y pasa el servidor a ocupado
      */
     ++num_clientes_espera;
-    estado_servidor = OCUPADO;
+    estado_servidores[servidor_libre] = OCUPADO;
+
+    
 
     /* Programa una salida ( servicio terminado ). */
-
-    tiempo_sig_evento[2] = tiempo_simulacion + expon(media_atencion);
+    tiempo_sig_evento[num_eventos + servidor_libre] = tiempo_simulacion + expon(media_atencion);
   }
 }
 
@@ -207,15 +245,17 @@ void salida(void) /* Funcion de Salida. */
 
   /* Revisa si la cola esta vacia */
 
-  if (num_entra_cola == 0) {
+  if (num_entra_cola == 0)
+  {
 
     /* La cola esta vacia, pasa el servidor a LIBRE y
     no considera el evento de salida*/
-    estado_servidor = LIBRE;
-    tiempo_sig_evento[2] = 1.0e+30;
+    estado_servidores[sig_tipo_evento - num_eventos] = LIBRE;
+    tiempo_sig_evento[sig_tipo_evento] = 1.0e+30;
   }
 
-  else {
+  else
+  {
 
     /* La cola no esta vacia, disminuye el numero de clientes en cola. */
     --num_entra_cola;
@@ -228,7 +268,7 @@ void salida(void) /* Funcion de Salida. */
 
     /*Incrementa el numero de clientes en espera, y programa la salida. */
     ++num_clientes_espera;
-    tiempo_sig_evento[2] = tiempo_simulacion + expon(media_atencion);
+    tiempo_sig_evento[sig_tipo_evento] = tiempo_simulacion + expon(media_atencion);
 
     /* Mueve cada cliente en la cola ( si los hay ) una posicion hacia adelante
      */
@@ -239,13 +279,19 @@ void salida(void) /* Funcion de Salida. */
 
 void reportes(void) /* Funcion generadora de reportes. */
 {
+
+  if (tiempo_simulacion > tiempo_simulacion_maxima)
+  {
+    tiempo_simulacion = tiempo_simulacion_maxima;
+  }
+
   /* Calcula y estima los estimados de las medidas deseadas de desempe�o */
   fprintf(resultados, "\n\nEspera promedio en la cola%11.3f minutos\n\n",
           total_de_esperas / num_clientes_espera);
   fprintf(resultados, "Numero promedio en cola%10.3f\n\n",
           area_num_entra_cola / tiempo_simulacion);
-  fprintf(resultados, "Uso del servidor%15.3f\n\n",
-          area_estado_servidor / tiempo_simulacion);
+  fprintf(resultados, "Uso promedio de los servidores%15.3f\n\n",
+          (sum(area_estado_servidor) / numero_servidores) / tiempo_simulacion);
   fprintf(resultados, "Tiempo de terminacion de la simulacion%12.3f minutos",
           tiempo_simulacion);
 }
@@ -266,7 +312,10 @@ void actualizar_estad_prom_tiempo(
   area_num_entra_cola += num_entra_cola * time_since_last_event;
 
   /*Actualiza el area bajo la funcion indicadora de servidor ocupado*/
-  area_estado_servidor += estado_servidor * time_since_last_event;
+  for (int i = 1; i <= numero_servidores; i++)
+  {
+    area_estado_servidor[i] += estado_servidores[i] * time_since_last_event;
+  }
 }
 
 float expon(float media) /* Funcion generadora de la exponencias */
@@ -274,4 +323,14 @@ float expon(float media) /* Funcion generadora de la exponencias */
   /* Retorna una variable aleatoria exponencial con media "media"*/
 
   return -media * log(lcgrand(1));
+}
+
+float sum(std::vector<float> array)
+{
+  float sum = 0;
+  for (int i = 1; i <= numero_servidores; i++)
+  {
+    sum += array[i];
+  }
+  return sum;
 }
