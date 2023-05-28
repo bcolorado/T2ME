637f27246d3f594035d4c34d1ffc6c765093c83d
Jhonatan Rodriguez <jhrodriguezi@unal.edu.co>
2023-05-28 14:01:01 -0500

    Erlang C by simulation added

--- src/MMN/main.cpp
+++ src/MMN/main.cpp
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <vector>
 
-#define LIMITE_COLA 100 /* Capacidad maxima de la cola */
+#define LIMITE_COLA 1000 /* Capacidad maxima de la cola */
 #define OCUPADO 1       /* Indicador de Servidor Ocupado */
 #define LIBRE 0         /* Indicador de Servidor Libre */
 
@@ -15,7 +15,7 @@ int sig_tipo_evento, num_clientes_espera, num_esperas_requerido, num_eventos,
 
 float area_num_entra_cola, media_entre_llegadas,
     media_atencion, tiempo_simulacion, tiempo_llegada[LIMITE_COLA + 1],
-    tiempo_ultimo_evento, total_de_esperas, tiempo_simulacion_maxima;
+    tiempo_ultimo_evento, total_de_esperas, tiempo_simulacion_maxima, tiempo_servidores_ocupados;
 
 std::vector<float> tiempo_sig_evento, area_estado_servidor;
 std::vector<int> estado_servidores;
@@ -115,6 +115,7 @@ void inicializar(void) /* Funcion de inicializacion. */
 
   num_entra_cola = 0;
   tiempo_ultimo_evento = 0.0;
+  tiempo_servidores_ocupados = 0.0;
 
   /* Inicializa los contadores estadisticos. */
 
@@ -184,8 +185,8 @@ void llegada(void) /* Funcion de llegada */
   {
     if (estado_servidores[i] == LIBRE)
     {
-      servidor_libre = i;
       servidor_desocupado_encontrado = true;
+      servidor_libre = i;
       break;
     }
   }
@@ -218,7 +219,6 @@ void llegada(void) /* Funcion de llegada */
 
   else
   {
-
     /*  El servidor esta LIBRE, por lo tanto el cliente que llega tiene tiempo
        de espera=0 (Las siguientes dos lineas del programa son para claridad, y
        no afectan el reultado de la simulacion ) */
@@ -230,8 +230,6 @@ void llegada(void) /* Funcion de llegada */
      */
     ++num_clientes_espera;
     estado_servidores[servidor_libre] = OCUPADO;
-
-    
 
     /* Programa una salida ( servicio terminado ). */
     tiempo_sig_evento[num_eventos + servidor_libre] = tiempo_simulacion + expon(media_atencion);
@@ -279,7 +277,6 @@ void salida(void) /* Funcion de Salida. */
 
 void reportes(void) /* Funcion generadora de reportes. */
 {
-
   if (tiempo_simulacion > tiempo_simulacion_maxima)
   {
     tiempo_simulacion = tiempo_simulacion_maxima;
@@ -288,12 +285,14 @@ void reportes(void) /* Funcion generadora de reportes. */
   /* Calcula y estima los estimados de las medidas deseadas de desempe�o */
   fprintf(resultados, "\n\nEspera promedio en la cola%11.3f minutos\n\n",
           total_de_esperas / num_clientes_espera);
-  fprintf(resultados, "Numero promedio en cola%10.3f\n\n",
+  fprintf(resultados, "Numero promedio en cola%10.6f\n\n",
           area_num_entra_cola / tiempo_simulacion);
   fprintf(resultados, "Uso promedio de los servidores%15.3f\n\n",
           (sum(area_estado_servidor) / numero_servidores) / tiempo_simulacion);
-  fprintf(resultados, "Tiempo de terminacion de la simulacion%12.3f minutos",
+  fprintf(resultados, "Tiempo de terminacion de la simulacion%12.3f minutos\n\n",
           tiempo_simulacion);
+  fprintf(resultados, "Valor formula C de Erlang (simulado)%12.6f",
+          tiempo_servidores_ocupados / tiempo_simulacion);
 }
 
 void actualizar_estad_prom_tiempo(
@@ -312,9 +311,19 @@ void actualizar_estad_prom_tiempo(
   area_num_entra_cola += num_entra_cola * time_since_last_event;
 
   /*Actualiza el area bajo la funcion indicadora de servidor ocupado*/
+  bool hacer_suma = true;
   for (int i = 1; i <= numero_servidores; i++)
   {
     area_estado_servidor[i] += estado_servidores[i] * time_since_last_event;
+    if (estado_servidores[i] == LIBRE && hacer_suma)
+    {
+      hacer_suma = false;
+    }
+  }
+
+  if (hacer_suma == true)
+  {
+    tiempo_servidores_ocupados += time_since_last_event;
   }
 }
 
